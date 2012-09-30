5d510fabc9113b5d6b82d17d512bc9e08c14915c
Chris Mytton <self@hecticjeff.net>
2012-09-30 20:58:16 +0100

    Don't use type in the name

--- procfile.go
+++ procfile.go
@@ -7,21 +7,21 @@ import (
 
 var procfileRegex = regexp.MustCompile("^([A-Za-z0-9_]+):\\s*(.+)$")
 
-type processType struct {
+type process struct {
 	command   string
 	arguments []string
 }
 
 // Parse will read the contents of a procfile and returns a map of process names
 // to commands.
-func Parse(procfile string) (procs map[string]processType) {
-	procs = make(map[string]processType)
+func Parse(procfile string) (procs map[string]process) {
+	procs = make(map[string]process)
 
 	for _, line := range strings.Split(procfile, "\n") {
 		if matches := procfileRegex.FindStringSubmatch(line); matches != nil {
 			name, command := matches[1], matches[2]
 			args := strings.Split(command, " ")
-			procs[name] = processType{args[0], args[1:]}
+			procs[name] = process{args[0], args[1:]}
 		}
 	}
 
