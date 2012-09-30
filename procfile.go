885ec150521efeafabb9ba6e4c293eac55fe4e3b
Chris Mytton <self@hecticjeff.net>
2012-09-30 22:01:34 +0100

    Expose Process struct in package

--- procfile.go
+++ procfile.go
@@ -7,21 +7,21 @@ import (
 
 var procfileRegex = regexp.MustCompile("^([A-Za-z0-9_]+):\\s*(.+)$")
 
-type process struct {
+type Process struct {
 	Command   string
 	Arguments []string
 }
 
 // Parse will read the contents of a procfile and returns a map of string names
-// to process structs.
-func Parse(procfile string) (procs map[string]process) {
-	procs = make(map[string]process)
+// to Process structs.
+func Parse(procfile string) (procs map[string]Process) {
+	procs = make(map[string]Process)
 
 	for _, line := range strings.Split(procfile, "\n") {
 		if matches := procfileRegex.FindStringSubmatch(line); matches != nil {
 			name, command := matches[1], matches[2]
 			args := strings.Split(command, " ")
-			procs[name] = process{args[0], args[1:]}
+			procs[name] = Process{args[0], args[1:]}
 		}
 	}
 
